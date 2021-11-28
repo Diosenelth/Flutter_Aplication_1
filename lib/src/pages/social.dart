2609c818ea68800d7563349a44f985842ebe6ccf
kevin <khernandez@alltic.co>
2021-11-28 16:06:04 -0500

    [DEVELOP] - se trabaja sobre dart social en tabs de actividades, social y estados, se implementa en tarjetas manejo dinamico con list generate y para el body variable widgetOptions en recorrido de sus elementos se hace desesctruturacion por cada tab list

--- lib/src/pages/social.dart
+++ lib/src/pages/social.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 
@@ -18,23 +19,29 @@ class Social extends StatefulWidget {
 
 class _PageState extends State<Social> {
   int _selectedIndex = 0;
+
   static const TextStyle optionStyle =
       TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
   
-  static const List<Widget> _widgetOptions = <Widget>[
-    Text(
-      'Index 0: Actividades',
-      style: optionStyle,
+  static List cardsActividad = List.generate(4, (i)=> cardActivity());
+  static List cardsSocial = List.generate(4, (i)=> cardSocialandEstado("SOCIAL"));
+   static List cardsEstado = List.generate(4, (i)=> cardSocialandEstado("ESTADO"));
+
+  final List<Widget> _widgetOptions = <Widget>[
+    ListView(
+        children: [...cardsActividad],
+      ),
+    ListView(
+        children: [
+          ..._textFields("SOCIAL"),
+          ...cardsSocial],
+    ), 
+    ListView(
+        children: [
+          ..._textFields("ESTADO"),
+          ...cardsEstado],
     ),
-    Text(
-      'Index 1: Social',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 2: Estados',
-      style: optionStyle,
-    ),
-    Text(
+    const Text(
       'Index 3: Menu',
       style: optionStyle,
     ),
@@ -56,6 +63,7 @@ class _PageState extends State<Social> {
         bottomNavigationBar:
         FlipBoxBar(
             selectedIndex: _selectedIndex,
+            navBarHeight: 40.0,
           items:[
             FlipBarItem(icon: const Icon(Icons.notifications),
              text: const Text("Actividades"),frontColor: Colors.red[900],
@@ -104,7 +112,163 @@ class _PageState extends State<Social> {
   );
 }
 
+  static List<Widget> _textFields(String vista){
+      return [
+        const SizedBox(height: 10),
+        texto(vista == "SOCIAL" ? "Publique contenido: " : "Publique estado:"),
+        const SizedBox(width: 10),      
+        fieldSocialandEstado(false,vista == "SOCIAL" ? "Social" : "Estado"),
+        const SizedBox(height: 10),
+        _buttonPublicar(vista == "SOCIAL" ? "SOCIAL" : "ESTADO"),
+        const SizedBox(height: 10),
+      ];
+  }
 
+  static Text texto(String texto){
+    return Text(
+        texto,
+        textAlign: TextAlign.justify,
+         style: const TextStyle(fontSize: 15),
+         );
+  }
+
+  static TextField fieldSocialandEstado(bool bool, String texto){
+    return TextField(    
+          keyboardType: TextInputType.multiline,
+          textInputAction: TextInputAction.newline,
+          minLines: 1,
+          maxLines: 5,
+          obscureText: bool,
+          decoration: InputDecoration(
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(15.0),
+            ),
+            labelText: texto,
+          ),
+        );
+  }
+
+
+  static Widget _buttonPublicar(String vista){
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+      publicarActionButton("Publicar", vista),
+    ],
+    );
+  }
+
+  static SizedBox publicarActionButton(String texto, String vista){
+    return SizedBox(
+      height: 40.0,
+      width: 100,
+      child:ElevatedButton(
+      onPressed: (()=>{
+         if (vista == "SOCIAL") {
+          print(vista)
+        }else {
+          print(vista)
+        }
+      }),
+      child: Text(texto)
+      ),
+    );
+  }
+
+static Card cardActivity(){
+      return Card(
+      margin: const EdgeInsets.all(10.0),
+      elevation: 5.0,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                TextButton(
+                  child: const Icon(Icons.note_outlined),
+                  onPressed: () {/* ... */},
+                ),
+                const SizedBox(width: 8),
+              ],
+            ),
+            const ListTile(
+              title: Text('Evento 1', textAlign: TextAlign.center),
+              subtitle: Text('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer est lacus, tincidunt vitae mi quis'
+              'eleifend porttitor felis. Cras vitae dolor vitae arcu pellentesque maximus a sit amet erat. Aliquam elementum pulvinar ex,'
+              'vehicula metus, at lobortis mi. Etiam luctus commodo rhoncus. Maecenas tincidunt elementum libero, eu vestibulum nisi egestas vitae.'),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: const <Widget>[
+                Text(
+                  'Experiencia: '
+                ),
+                //const SizedBox(width: 25),
+                Text(
+                  'Pago: '
+                ),
+              ],
+            ),
+          ],
+        ),
+      );
+  }
+
+  static Card cardSocialandEstado(String vista){
+    if (vista == "SOCIAL") {
+      return Card(
+      margin: const EdgeInsets.all(10.0),
+      elevation: 5.0,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                TextButton(
+                  child: const Icon(Icons.colorize_rounded),
+                  onPressed: () {/* ... */},
+                ),
+                const SizedBox(width: 8),
+              ],
+            ),
+            const ListTile(
+              title: Text('Fulano 1', textAlign: TextAlign.center),
+              subtitle: Text('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer est lacus, tincidunt vitae mi quis'
+              'vehicula metus, at lobortis mi. Etiam luctus commodo rhoncus. Maecenas tincidunt elementum libero, eu vestibulum nisi egestas vitae.'),
+            ),
+          ],
+        ),
+      );
+    }else{
+       return Card(
+      margin: const EdgeInsets.all(10.0),
+      elevation: 5.0,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                TextButton(
+                  child: const Icon(Icons.colorize_rounded),
+                  onPressed: () {/* ... */},
+                ),
+                const SizedBox(width: 8),
+              ],
+            ),
+            const ListTile(
+              title: Text('Fulano 2', textAlign: TextAlign.center),
+              subtitle: Text('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer est lacus, tincidunt vitae mi quis'),
+            ),
+          ],
+        ),
+      );
+    } 
+  }
+
+    
 
     void _onItemTapped(int index) {
     setState(() {
